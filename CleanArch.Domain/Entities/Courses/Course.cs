34b16620117cdab0fa0067d5e1291f0d1d7027c4
Given Kibanza <given@wbwr.io>
2025-09-19 16:40:45 +0200

    initialized domain project and added Course entity

---
+++ CleanArch.Domain/Entities/Courses/Course.cs
@@ -0,0 +1,9 @@
+namespace CleanArch.Domain.Entities.Courses;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
